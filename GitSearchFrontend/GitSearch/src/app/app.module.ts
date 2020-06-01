32ec128e9c21235e51bf19a71bfacd09fc9e2f5c
inamjr <ikeogu56@gmail.com>
2020-05-31 19:28:47 -0500

    bootstrap added
    Added some style to the front end

--- GitSearchFrontend/GitSearch/src/app/app.module.ts
+++ GitSearchFrontend/GitSearch/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {SerchService} from "./serch.service";
+import { SerchService } from './serch.service';
 import { DbinfoComponent } from './dbinfo/dbinfo.component';
 import { ItemsComponent } from './items/items.component';
-import { HeaderComponent } from './header/header.component'
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,14 +18,15 @@ import { HeaderComponent } from './header/header.component'
     DbinfoComponent,
     ItemsComponent,
     HeaderComponent,
-   
   ],
   imports: [
     BrowserModule,
+    MatSliderModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [SerchService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
